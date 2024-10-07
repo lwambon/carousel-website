5de95c467502647357bfa2eb3a894fc7940164ba
lwambon <lwambonyaga@gmail.com>
2024-10-07 14:58:22 +0300

    carousel website

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 let slideIndex = 1;
-contentContainer(slideIndex);
+document.addEventListener("DOMContentLoaded", function () {
+  contentContainer(slideIndex);
+});
 
+window.plusSlides = function (n) {
+  contentContainer((slideIndex += n));
+};
 
-function plusSlides(n) {
-    contentContainer(slideIndex += n);
-}
-
-
-function currentSlide(n) {
-    contentContainer(slideIndex = n);
-}
+window.currentSlide = function (n) {
+  contentContainer((slideIndex = n));
+};
 
 function contentContainer(n) {
   let i;
   let slides = document.getElementsByClassName("content-container");
   let dots = document.getElementsByClassName("dot");
-  
+
   if (n > slides.length) {
     slideIndex = 1;
   }
   if (n < 1) {
     slideIndex = slides.length;
   }
-  
+
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
